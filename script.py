7e38f085cea9aaefd22040b1c7f91acabc3c4b55
liuvis05 <94665901+liuvis05@users.noreply.github.com>
2022-06-23 13:57:34 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,16 +1,17 @@
+import numpy as np
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 
 #2. Read csv
-df = pd.read_csv('charcters_stats.csv')
-print(df.to_string())
+pd.read_csv('charcters_stats.csv')
 
 #3. Show first records from csv
-print(df.head())
+df = pd.read_csv('charcters_stats.csv')
+df.head()
 
 #4. Show number of rows and columns
-print(df.info())
+df.shape
  
 # Cleaning Empty Cells
 new_df = df.dropna()
@@ -21,97 +22,136 @@ print(new_df.to_string())
 df['Alignment'].value_counts().to_frame()
 
 #6. Find out only good alignment holders superheroes
-df['Alignment'].value_counts().to_frame().loc['good']
+df.loc[df['Alignment'] == 'good']
 
 #7. Show first five records which you found in point 6
-df[["Name","Alignment"]].loc[df['Alignment'] == 'good'].head(5)
+df.loc[df['Alignment'] == 'good'].head(5)
 
 #8. Show top five records having top speed of heroes of good alignment
-df[["Name","Alignment","Speed"]].loc[df['Alignment'] == 'good'].nlargest(5,'Speed')
+df.loc[df['Alignment'] == 'good'].nlargest(5,'Speed')
 
 #9. Show 5 records of super heroes who have maximum power of good alignment
-df[["Name","Alignment","Power"]].loc[df['Alignment'] == 'good'].nlargest(5,'Power')
+df.loc[df['Alignment'] == 'good'].nlargest(5,'Power')
 
 #10. Find out how many super heroes are there with power 100 of good alignment
 super_heros=df[["Name","Alignment","Power"]].loc[df['Alignment'] == 'good'].Power.value_counts()[100]
 print(f"There are {super_heros} super heros with power 100 of good alignment")
 
-###################11. Shape them what you got in point 10
-
+#11. Shape them what you got in point 10
+df.loc[(df['Alignment'] == 'good') & (df['Power'] == 100)].shape
 
 #12. Show all records from point 10
 df.loc[(df['Alignment'] == 'good') & (df['Power'] == 100)]
 
 #13. Retrieve total of first five records of max power of good alignment super heroes
-# Show 5 records of super heroes who have maximum power of good alignment(same???????)
-df[["Name","Alignment","Power"]].loc[df['Alignment'] == 'good'].nlargest(5,'Power')
+df.loc[df['Alignment'] == 'good'].nlargest(5,'Total')
 
 
-##################14. Draw a bar plot of all super heroes who are having good alignment and max power of top five only , 
+#14. Draw a bar plot of all super heroes who are having good alignment and max power of top five only , 
 #take same object of point 13 , 
 #show name and total in plot with green bars
 
-max_powers=df[["Name","Alignment","Power"]].loc[df['Alignment'] == 'good'].nlargest(5,'Power')
-#print(max_powers)
+max_powers=df.loc[df['Alignment'] == 'good'].nlargest(5,'Total')
 
 
-plt.bar([max_powers,'Name'],max_powers,['Alignment'], color='green')
-plt.title('Top 10 Countries and Number of Players', fontsize=16)
-plt.xlabel('Country', fontsize=16)
-plt.ylabel('Number of Players', fontsize=16)
-plt.grid(True)
+plt.bar(max_powers['Name'],max_powers['Total'], color='green')
+plt.title('Top 5 ', fontsize=16)
+plt.xlabel('Name', fontsize=16)
+plt.ylabel('Total', fontsize=16)
 plt.show()
 
+#####extra
+max_powers=df.loc[df['Alignment'] == 'good'].nlargest(5,'Speed')
+
+
+plt.bar(max_powers['Name'],max_powers['Speed'], color='blue')
+plt.title('Top 5 ', fontsize=16)
+plt.xlabel('Name', fontsize=16)
+plt.ylabel('Speed', fontsize=16)
+plt.show()
+
+#####Extra 
+max_powers=df.loc[df['Alignment'] == 'good'].nlargest(5,'Power')
+
+
+plt.bar(max_powers['Name'],max_powers['Power'], color='red')
+plt.title('Top 5 ', fontsize=16)
+plt.xlabel('Name', fontsize=16)
+plt.ylabel('Power', fontsize=16)
+plt.show()
+
+
 #15. Extract villains having bad alignment
-df[["Name","Alignment"]].loc[df['Alignment'] == 'bad']
+df.loc[df['Alignment'] == 'bad']
 
 #16. Show first five records of point 15
-df[["Name","Alignment"]].loc[df['Alignment'] == 'bad'].head(5)
+df.loc[df['Alignment'] == 'bad'].head(5)
 
 #17. Show top five fastest super villains in terms of super speed
-df[["Name","Alignment","Speed"]].loc[df['Alignment'] == 'bad'].nlargest(5,'Speed')
+df.loc[df['Alignment'] == 'bad'].nlargest(5,'Speed')
+
+#####extras
+#Intelligence	Strength	Speed	Durability	Power	Combat	Total
+villians=df[["Speed"]].loc[df['Alignment'] == 'bad'].head()
+
+ax = villians.plot(
+    #style="ro--",
+    title="Villians",
+    xlabel="Total",
+    ylabel="Speed",
+)
 
 #18. Top five super villains in terms of intelligence
-df[["Name","Alignment","Intelligence"]].loc[df['Alignment'] == 'bad'].nlargest(5,'Intelligence')
+df.loc[df['Alignment'] == 'bad'].nlargest(5,'Intelligence')
 
 #19. Show who is most dangerous super villain after calculating their total (top 5 only)
-df[["Name","Alignment","Total"]].loc[df['Alignment'] == 'bad'].nlargest(5,'Total')
+df.loc[df['Alignment'] == 'bad'].nlargest(5,'Total')
 
 #20. Draw a histogram for speed of super heroes having fig size 10,5 , 
 #provide speed in histogram for only good alignment super heroes ,title should be "distribution of speed" , xlabel should be "speed"
 
-heroes=df[["Name","Alignment","Speed"]].loc[df['Alignment'] == 'good'].nlargest(33,'Speed')
-# df.hist(column='Speed');
+heroes=df[["Alignment","Speed"]].loc[df['Alignment'] == 'good']
 
 heroes.plot(kind='hist',
-        alpha=0.7,
-        bins=30,
-        title='Histogram Of Speed',
+        #alpha=0.7,
+        #bins=30,
+        title='Distribution Of Speed',
         rot=45,
         grid=True,
         figsize=(10,5),
         fontsize=15, 
-        color=['#A0E8AF'])#color=['#A0E8AF', '#FFCF56'])
+        color=['#A0E8AF'])
 plt.xlabel('Speed')
-plt.ylabel("Distribution of speed");
+plt.ylabel("Heros");
 
 #21. Draw a histogram for combat of super villains having fig size 10,5 , 
 #provide combat in histogram for only bad alignment super heroes ,title should be "distribution of combat" ,xlabel should be "combat" *** 
 
-super_heros=df[["Name","Alignment","Power"]].loc[df['Alignment'] == 'bad'].Power.value_counts()[100]
-
-print(f"There are {super_heros} super heros with power 100 of bad alignment")
-
-villians=df[["Name","Alignment","Total"]].loc[df['Alignment'] == 'bad'].nlargest(20,'Total')
+villians=df[["Name","Alignment","Combat"]].loc[df['Alignment'] == 'bad']
 
 villians.plot(kind='hist',
-        alpha=0.7,
-        bins=30,
-        title='Histogram Of Combat',
+        title='Distribution Of Combat',
         rot=45,
         grid=True,
         figsize=(10,5),
         fontsize=15, 
-        color=['#FFCF56'])#color=['#A0E8AF', '#FFCF56'])
+        color=['#FFCF56'])
 plt.xlabel('Combat')
-plt.ylabel("Distribution of combat");
+plt.ylabel("Villains");
+
+#Name	Alignment	Intelligence	Strength	Speed	Durability	Power	Combat	Total
+df.hist(figsize=(15, 10), color='pink')
+
+villians=df[["Speed"]].loc[df['Alignment'] == 'bad']
+villians.plot.hist(y='Speed', figsize=(10, 5), color='yellow')
+plt.show()
+
+#Intelligence	Strength	Speed	Durability	Power	Combat	Total
+df[["Name", "Intelligence","Strength","Power","Speed","Durability","Combat"]].loc[df['Alignment'] == 'bad'].nlargest(5,'Durability')
+
+#Intelligence	Strength	Speed	Durability	Power	Combat	Total
+durability=df[["Intelligence","Strength","Power","Speed","Durability","Combat"]].loc[df['Alignment'] == 'bad'].nlargest(5,'Durability')
+durability.plot.barh(figsize=(15, 10))
+
+durability=df[["Intelligence","Strength","Power","Speed","Durability","Combat"]].loc[df['Alignment'] == 'bad'].head()
+durability.plot.area()
